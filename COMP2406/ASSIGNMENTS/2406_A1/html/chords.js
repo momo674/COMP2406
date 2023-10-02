2a9bc46b4c9d282f6915a097150c4e5c1b976219
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2023-10-01 22:50:16 -0400

    work

--- COMP2406/ASSIGNMENTS/2406_A1/html/chords.js
+++ COMP2406/ASSIGNMENTS/2406_A1/html/chords.js
@@ -72,70 +72,139 @@ function parseChordProFormat(chordProLinesArray) {
 
 
 let notes_sharp = ['A',	'A#', 'B',  'C', 'C#',	'D',	'D#',	'E',	'F',	'F#',	'G',	'G#']
+let notes_minor = ['A'	,'Bb','B',	,'C',	'Db',	'D',	'Eb',	'E',	'F',	'Gb',	'G'	,'Ab']
 let counter = 0;
 function transposeUp(){
+  
     let elms = document.querySelectorAll("[id='noteline']");
+   if (elms.length == 0) {return;}
+    
+    
     for(var i = 0; i < elms.length; i++){
         elms[i].style.color='red';
         let b = []
-
+        
         for (let x = 0 ; x < notes_total[i].length; x++) {
-          //console.log(notes_total[i][x])
+          
           if (!b.includes(notes_total[i][x])) {
             b.push(notes_total[i][x])
           }
 
 
         }
-        //console.log(elms[i].innerHTML)
-        //console.log(b)
+        
         for (let n = 0; n < b.length; n++) {
+
+          
+          
+          
+
           let cur_note = b[n]
           let new_note = transposeUpConvert(cur_note)
-          console.log(cur_note + ' ' + new_note);
-          notes_total[i][n] = new_note
+          
+          
+
+          tmp = replaceAll(notes_total[i],cur_note,new_note);
+          notes_total[i] = tmp
+
           elms[i].innerHTML = elms[i].innerHTML.replaceAll(cur_note, new_note)
+          //console.log(elms[i].innerHTML + " " + i + cur_note + " " + new_note)
+          
         }
-        console.log(elms[i].innerHTML)
-
         
-        // console.log(elms[i]);
-        // console.log(notes_total[i]);
-    }
-
-    if (counter > 12 ) {
-
-        for(var i = 0; i < elms.length; i++){
         
-            elms[i].style.color='green';
-            
-        }
-        counter = 0;
+        
+    
     }
-    console.log('\n')
-    counter++; 
+    counter++;
+    if (counter % 12 == 0 && counter!=0) {
+      
+      counter = 0;
+    
+    
+
+    for(var i = 0; i < elms.length; i++){
+      
+      elms[i].style.color='green';
+      
+    }
+    return
+    
+   
+    
+  }
+    
+    
     
 
 }
-  function removeDupes(a) {
-    let b = []
-
-    for (let i = 0; i<a.length; i++) {
-      if (b.includes(a[i])) {b.push(a[i])}
+function replaceAll(arr, old, n) {
+  if (arr === undefined) {return []}
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] == old) {
+      arr[i] = n;
     }
-
-    return b
   }
- function transposeUpConvert(note) {
-  // if (note.includes('/')) { //double note
 
-  // }
+  return arr;
+}
 
 
-  // else if (note.includes('b')) { //minor
+function transposeUpConvert(note) {
+  if (note.includes('/')) { //double note
+    let note1 = ''
+    let note2 = ''
+    let note1_select = true;
+    for (let i = 0; i < note.length; i++) {
+      
+      if (note.charAt(i) == '/') {
+        note1_select = false;
+        continue
+      }
+      if (note1_select) {
+        note1+= note.charAt(i)
+      }
 
-  // }
-  if (true) { //regular note or sharp
+      else {
+        note2+= note.charAt(i)
+      }
+
+    }
+    note_re = transposeUpConvert(note1) + "/" + transposeUpConvert(note2);
+    return note_re
+   }
+
+
+  else if (note.includes('b')) { //minor
+    let note_to_change = '';
+    let note_add_end = '';
+    let final_note = '';
+
+    for (let i = 0; i < note.length; i++) {
+      let character = note.charAt(i)
+      if (i == 0) {
+        note_to_change += character;
+      }
+      else if (i == 1 && character == 'b') {
+        note_to_change += character;
+      }
+      else {
+        note_add_end += character;
+      }
+    }
+  
+  
+    
+    let index = notes_minor.indexOf(note_to_change);
+    index++; 
+    
+    
+    note_to_change = notes_minor[index % 12];
+    final_note = note_to_change + note_add_end;
+    return final_note;
+   }
+  
+  else { //regular note or sharp
     //seperate our note
 
     let note_to_change = '';
@@ -179,12 +248,173 @@ function transposeUp(){
   }
  }
 
- //console.log(transposeUpConvert('D'));
- 
+function transposeDown(){
+  
+  let elms = document.querySelectorAll("[id='noteline']");
+ if (elms.length == 0) {return;}
+  
+  
+  for(var i = 0; i < elms.length; i++){
+      elms[i].style.color='red';
+      let b = []
+      
+      for (let x = 0 ; x < notes_total[i].length; x++) {
+        
+        if (!b.includes(notes_total[i][x])) {
+          b.push(notes_total[i][x])
+        }
 
+
+      }
+      
+      for (let n = 0; n < b.length; n++) {
+
+        
+        
+        
+
+        let cur_note = b[n]
+        let new_note = transposeDownConvert(cur_note)
+        
+        
+
+        tmp = replaceAll(notes_total[i],cur_note,new_note);
+        notes_total[i] = tmp
+
+        elms[i].innerHTML = elms[i].innerHTML.replaceAll(cur_note, new_note)
+        //console.log(elms[i].innerHTML + " " + i + cur_note + " " + new_note)
+        
+      }
+      
+      
+      
+  
+  }
+  counter--;
+  if (counter % 12 == 0 && counter!=0) {
+    
+    counter = 0;
+  
+  
+
+  for(var i = 0; i < elms.length; i++){
+    
+    elms[i].style.color='green';
+    
+  }
+  return
+  
+ 
+  
+}
+  
+  
+  
+
+}
+
+function transposeDownConvert(note) {
+  if (note.includes('/')) { //double note
+    let note1 = ''
+    let note2 = ''
+    let note1_select = true;
+    for (let i = 0; i < note.length; i++) {
+      
+      if (note.charAt(i) == '/') {
+        note1_select = false;
+        continue
+      }
+      if (note1_select) {
+        note1+= note.charAt(i)
+      }
+
+      else {
+        note2+= note.charAt(i)
+      }
+
+    }
+    note_re = transposeUpConvert(note1) + "/" + transposeUpConvert(note2);
+    return note_re
+   }
+
+
+  else if (note.includes('b')) { //minor
+    let note_to_change = '';
+    let note_add_end = '';
+    let final_note = '';
+
+    for (let i = 0; i < note.length; i++) {
+      let character = note.charAt(i)
+      if (i == 0) {
+        note_to_change += character;
+      }
+      else if (i == 1 && character == 'b') {
+        note_to_change += character;
+      }
+      else {
+        note_add_end += character;
+      }
+    }
+  
+  
+    
+    let index = notes_minor.indexOf(note_to_change);
+    index--; 
+    
+    
+    note_to_change = notes_minor[index % 12];
+    final_note = note_to_change + note_add_end;
+    return final_note;
+   }
+  
+  else { //regular note or sharp
+    //seperate our note
+
+    let note_to_change = '';
+    let note_add_end = '';
+    let final_note = '';
+    
+    if (note.length == 1) {
+      note_to_change = note;
+      let index = notes_sharp.indexOf(note_to_change);
+      index--; 
+    
+    
+      note_to_change = notes_sharp[index % 12];
+      final_note = note_to_change + note_add_end;
+      return final_note;
+    }
+    
+  
+    for (let i = 0; i < note.length; i++) {
+      let character = note.charAt(i)
+      if (i == 0) {
+        note_to_change += character;
+      }
+      else if (i == 1 && character == '#') {
+        note_to_change += character;
+      }
+      else {
+        note_add_end += character;
+      }
+    }
+  
+  
+    
+    let index = notes_sharp.indexOf(note_to_change);
+    index--; 
+    
+    
+    note_to_change = notes_sharp[index % 12];
+    final_note = note_to_change + note_add_end;
+    return final_note;
+  }
+ }
 
 
 document.getElementById('up').addEventListener("click", transposeUp);
+document.getElementById('down').addEventListener("click", transposeDown);
+
 
 
 
