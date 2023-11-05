7fea16566445b6e3701320ebb4375f04376e8930
Mohamad Radaideh <mohparrot@gmail.com>
2023-11-05 11:41:53 -0500

    Update chatClient.js

--- ASSIGNMENTS/2406_A3/html/chatClient.js
+++ ASSIGNMENTS/2406_A3/html/chatClient.js
@@ -32,6 +32,8 @@ socket.on('serverSays', function(message) {
     let potential_user = raw_message[1]
     let potential_sender = raw_message[0];
     let potential_message = raw_message[2];
+    let potential_multiple_users = potential_user.split(", ")
+  console.log(potential_multiple_users)
 
 
     //check if we are not the sender or the reciever, then we cannot see this message
